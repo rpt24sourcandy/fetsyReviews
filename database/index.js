10e1a0dc4e38da35267e41b0c41021a0ee9ef71c
Tina-Luk <tina.luk.tina@gmail.com>
2020-11-14 09:26:14 -0800

    set up database

---
+++ database/index.js
@@ -0,0 +1,3 @@
+const db = require('./utils/database');
+const Review = require('./utils/seed');
+const Item = require('./utils/seed');
---
+++ database/utils/seed.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require('sequelize');
+
+const db = require('./database');
+
+var Item = db.define('Item', {
+	id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+	item_name: Sequelize.STRING,
+});
+
+var Review = db.define('Review', {
+	id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+	customer_name: Sequelize.STRING,
+	date_of_review: Sequelize.STRING,
+	rating: { type: Sequelize.INTEGER, min: 0, max: 5 },
+	review_content: Sequelize.STRING,
+	image_url: Sequelize.STRING,
+});
+
+Review.belongsTo(Item);
+Item.hasMany(Review);
+
+Item.sync({ force: true });
+Review.sync({ force: true });
+
+exports.Item = Item;
+exports.Review = Review;
