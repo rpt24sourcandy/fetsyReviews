1ff77104281bc8d12215d2d2535d148162fede3f
Tina-Luk <tina.luk.tina@gmail.com>
2020-11-14 12:52:19 -0800

    Create item data set and seed to database

--- database/utils/seed.js
+++ database/utils/seed.js
@@ -2,6 +2,9 @@ const { Sequelize } = require('sequelize');
 
 const db = require('./database');
 
+db.query('DROP TABLE IF EXISTS ITEMS');
+db.query('DROP TABLE IF EXISTS REVIEWS');
+
 var Item = db.define('Item', {
 	id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
 	item_name: Sequelize.STRING,
@@ -16,13 +19,51 @@ var Review = db.define('Review', {
 	image_url: Sequelize.STRING,
 });
 
-Review.belongsTo(Item);
 Item.hasMany(Review);
+Review.belongsTo(Item);
 
-Item.findOrCreate({ where: { item_name: 'Wine RANDOM Face Mask with clear and red rhinestones' } });
+Item.sync();
+Review.sync();
 
-Item.sync({ force: true });
-Review.sync({ force: true });
+const itemsData = [
+	{
+		item_name: 'Wine RANDOM Face Mask with clear and red rhinestones',
+	},
+	{
+		item_name: 'washable Face Mask covering with glitter rose gold glitter Custom made, Designer FaceMask, with NO pocket for filter, mask holder lanyard',
+	},
+	{
+		item_name: 'Hanging with my Gnomies face mask made of clear, green and red rhinestones',
+	},
+	{
+		item_name: 'FaceMask mask holder lanyard, washable Face Mask covering with glitter rose gold glitter Custom made, Designer with NO pocket for filter',
+	},
+	{
+		item_name: 'Face Shield with Flippable Face Shield - PPE - Comfortable Elastic Head Band - Full Cover Safety Cover',
+	},
+	{
+		item_name: 'Winter Soldier type of PPE covid -19 mask',
+	},
+	{
+		item_name: 'Halloween Collection, Holiday, Covid PPE, Fall, Thanksgiving, Scrub cap, surgical cap, nurse, doctor, dentist, men, women, buttons,',
+	},
+	{
+		item_name: 'HStar Wars Collection, Mandalorian, Covid PPE, Yoda, The Child, Scrub cap, surgical cap, nurse, doctor, dentist, men, women, buttons,',
+	},
+	{
+		item_name: 'FTP Face Mask Covering Reusable | Hype Beast Hip-Hop Rap Street Wear SuicideBoys Travis Scott Cactus Jack COVID PPE Designer Brand',
+	},
+	{
+		item_name: 'Dry-Fit Moisture-Wicking Adult Face Masks',
+	},
+];
+
+const addItem = (name) => {
+	Item.findOrCreate({ where: { item_name: name } });
+};
+itemsData.forEach((item) => {
+	addItem(item.item_name);
+});
 
 exports.Item = Item;
 exports.Review = Review;
