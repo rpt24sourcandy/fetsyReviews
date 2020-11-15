0f829115d882f65de8d6d5cf5db3340dc089bc8c
Tina-Luk <tina.luk.tina@gmail.com>
2020-11-14 18:37:18 -0800

    Add eslint airbnb guide

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var controller = require('./controller');
+const express = require('express');
+// const controller = require('./controller');
 const app = express();
 const port = 3002;
 
@@ -8,5 +8,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('client/dist'));
 
 app.listen(port, () => {
+	// eslint-disable-next-line no-console
 	console.log(`Listening at http://localhost:${port}`);
 });
