c06bf25e5052250bea7f77a097bf7600abe4592f
Tina-Luk <tina.luk.tina@gmail.com>
2020-11-16 20:46:07 -0800

    working API review call working on unit mocking test

--- spec/test.js
+++ spec/test.js
@@ -1,5 +1,12 @@
-const sum = require('./sum');
+// const axios = require('axios');
+// const getReviews = require('./index');
+// jest.mock('axios');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+// describe('/GET Reviews based on item ID', () => {
+// 	it('get customer name of the review', async () => {
+// 		const reviews = await getReviews(2); // Run the function
+// 		// console.log(reviews);
+// 		// expect(reviews[0].customer_name).toEqual('Kazza'); // Make an assertion on the result
+// 		expect(axios.request).toHaveBeenCalled();
+// 	});
+// });
