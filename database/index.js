278bf757cd8cc67e4ff86a233408117b5f616340
Tina-Luk <tina.luk.tina@gmail.com>
2020-11-16 20:15:58 -0800

    API Call to get review for certain item id

--- database/index.js
+++ database/index.js
@@ -1,3 +1,32 @@
-const db = require('./utils/database');
-const Review = require('./utils/seed');
-const Item = require('./utils/seed');
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize('REVIEWS', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+const Item = db.define('Item', {
+  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+  item_name: Sequelize.STRING,
+});
+
+const Review = db.define('Review', {
+  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+  customer_name: Sequelize.STRING,
+  date_of_review: Sequelize.STRING,
+  rating: { type: Sequelize.INTEGER, min: 0, max: 5 },
+  review_content: Sequelize.STRING,
+  image_url: Sequelize.STRING,
+  item_option: Sequelize.STRING,
+});
+
+Item.hasMany(Review);
+Review.belongsTo(Item);
+
+Item.sync();
+Review.sync();
+
+exports.Item = Item;
+exports.Review = Review;
+
+exports.db = db;
--- database/utils/reviewsSeed.js
+++ database/utils/reviewsSeed.js
@@ -1,110 +1,109 @@
 const faker = require('faker');
 
 const reviewsData = [
-	{
-		customer_name: 'Jennifer Stewart',
-		date_of_review: 'Oct 31, 2020',
-		rating: 5,
-		review_content: '',
-		image_url: 'https://source.unsplash.com/random',
-		ItemId: 1,
-		item_option: 'Hot Pink',
-	},
-	{
-		customer_name: 'thebec69',
-		date_of_review: 'Nov 11, 2020',
-		rating: 5,
-		review_content: '',
-		image_url: 'https://source.unsplash.com/random',
-		ItemId: 1,
-		item_option: 'Black',
-	},
-	{
-		customer_name: 'Beverly Smith',
-		date_of_review: 'Nov 10, 2020',
-		rating: 5,
-		review_content: 'Really love this mask in black! I received a lot of compliments. It was delivered on time .',
-		image_url: 'https://source.unsplash.com/random',
-		ItemId: 1,
-		item_option: 'Black',
-	},
-	{
-		customer_name: 'dancinladyy',
-		date_of_review: 'Nov 3, 2020',
-		rating: 3,
-		review_content: 'NEEDS TO BE LONGER SO IT COMPLETELY COVERS CHIN',
-		image_url: 'https://source.unsplash.com/random',
-		ItemId: 1,
-		item_option: 'Black',
-	},
-	{
-		customer_name: 'Kazza',
-		date_of_review: 'Nov 11, 2020',
-		rating: 5,
-		review_content: 'Speedy delivery Item nice Recommend',
-		image_url: 'https://source.unsplash.com/random',
-		ItemId: 2,
-		item_option: 'Christmas tree+bow',
-	},
-	{
-		customer_name: 'Angela Naylor',
-		date_of_review: 'Oct 21, 2020',
-		rating: 5,
-		review_content: 'Absolutely beautiful will definitely be purchasing some more . Delivery time outstanding so quick thank you so much xx 💜',
-		image_url: 'https://source.unsplash.com/random',
-		ItemId: 2,
-		item_option: 'Christmas tree+bow',
-	},
-	{
-		customer_name: 'ruthiebaby1619',
-		date_of_review: 'Nov 9, 2020',
-		rating: 5,
-		review_content: "This mask is fabulous! Glitter design at both sides. Very comfortable. Can't wait till December to wear it!",
-		image_url: 'https://source.unsplash.com/random',
-		ItemId: 2,
-		item_option: 'Christmas tree+bow',
-	},
-	{
-		customer_name: 'Laura Hendricks',
-		date_of_review: 'Oct 20, 2020',
-		rating: 5,
-		review_content: 'Very comfortable and breathable without being right up against my mouth',
-		image_url: 'https://source.unsplash.com/random',
-		ItemId: 2,
-		item_option: '06. grey rose bows',
-	},
+  {
+    customer_name: 'Jennifer Stewart',
+    date_of_review: 'Oct 31, 2020',
+    rating: 5,
+    review_content: '',
+    image_url: 'https://source.unsplash.com/random',
+    ItemId: 1,
+    item_option: 'Hot Pink',
+  },
+  {
+    customer_name: 'thebec69',
+    date_of_review: 'Nov 11, 2020',
+    rating: 5,
+    review_content: '',
+    image_url: 'https://source.unsplash.com/random',
+    ItemId: 1,
+    item_option: 'Black',
+  },
+  {
+    customer_name: 'Beverly Smith',
+    date_of_review: 'Nov 10, 2020',
+    rating: 5,
+    review_content: 'Really love this mask in black! I received a lot of compliments. It was delivered on time .',
+    image_url: 'https://source.unsplash.com/random',
+    ItemId: 1,
+    item_option: 'Black',
+  },
+  {
+    customer_name: 'dancinladyy',
+    date_of_review: 'Nov 3, 2020',
+    rating: 3,
+    review_content: 'NEEDS TO BE LONGER SO IT COMPLETELY COVERS CHIN',
+    image_url: 'https://source.unsplash.com/random',
+    ItemId: 1,
+    item_option: 'Black',
+  },
+  {
+    customer_name: 'Kazza',
+    date_of_review: 'Nov 11, 2020',
+    rating: 5,
+    review_content: 'Speedy delivery Item nice Recommend',
+    image_url: 'https://source.unsplash.com/random',
+    ItemId: 2,
+    item_option: 'Christmas tree+bow',
+  },
+  {
+    customer_name: 'Angela Naylor',
+    date_of_review: 'Oct 21, 2020',
+    rating: 5,
+    review_content: 'Absolutely beautiful will definitely be purchasing some more . Delivery time outstanding so quick thank you so much xx 💜',
+    image_url: 'https://source.unsplash.com/random',
+    ItemId: 2,
+    item_option: 'Christmas tree+bow',
+  },
+  {
+    customer_name: 'ruthiebaby1619',
+    date_of_review: 'Nov 9, 2020',
+    rating: 5,
+    review_content: "This mask is fabulous! Glitter design at both sides. Very comfortable. Can't wait till December to wear it!",
+    image_url: 'https://source.unsplash.com/random',
+    ItemId: 2,
+    item_option: 'Christmas tree+bow',
+  },
+  {
+    customer_name: 'Laura Hendricks',
+    date_of_review: 'Oct 20, 2020',
+    rating: 5,
+    review_content: 'Very comfortable and breathable without being right up against my mouth',
+    image_url: 'https://source.unsplash.com/random',
+    ItemId: 2,
+    item_option: '06. grey rose bows',
+  },
 ];
 
 const addFakeReview = () => {
-	const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-	let name = faker.name.findName();
-	let d = faker.date.past();
-	let date = `${monthNames[d.getMonth()]} ${d.getDay()}, ${d.getFullYear()}`;
-	let rating = faker.random.number({
-		min: 1,
-		max: 5,
-	});
-	let content = faker.lorem.sentence();
-	let options = faker.lorem.words(3);
-	let itemId = faker.random.number({
-		min: 1,
-		max: 10,
-	});
-	let json = {
-		customer_name: name,
-		date_of_review: date,
-		rating: rating,
-		review_content: content,
-		image_url: 'https://source.unsplash.com/random',
-		ItemId: itemId,
-		item_option: options,
-	};
-	reviewsData.push(json);
+  const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+  const name = faker.name.findName();
+  const d = faker.date.past();
+  const date = `${monthNames[d.getMonth()]} ${d.getDay()}, ${d.getFullYear()}`;
+  const rating = faker.random.number({
+    min: 1,
+    max: 5,
+  });
+  const content = faker.lorem.sentence();
+  const options = faker.lorem.words(3);
+  const itemId = faker.random.number({
+    min: 1,
+    max: 10,
+  });
+  const json = {
+    customer_name: name,
+    date_of_review: date,
+    rating,
+    review_content: content,
+    image_url: 'https://source.unsplash.com/random',
+    ItemId: itemId,
+    item_option: options,
+  };
+  reviewsData.push(json);
 };
 
-for (let i = 0; i < 200; i++) {
-	addFakeReview();
+for (let i = 0; i < 200; i += 1) {
+  addFakeReview();
 }
-console.log(reviewsData);
 
 module.exports = reviewsData;
