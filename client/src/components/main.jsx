b77b30a4a5df2f7850b35d2c999524b8d1a55c37
Tina-Luk <tina.luk.tina@gmail.com>
2020-11-18 19:12:43 -0800

    Axios get request for item id

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -2,36 +2,36 @@ import React from 'react';
 import axios from 'axios';
 
 class Main extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			reviews: [],
-			itemId: 1,
-		};
-		this.getReviews = this.getReviews.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: [],
+      itemId: 1,
+    };
+    this.getReviews = this.getReviews.bind(this);
+  }
 
-	componentDidMount() {
-		this.getReviews(this.state.itemId);
-	}
+  componentDidMount() {
+    this.getReviews(this.state.itemId);
+  }
 
-	getReviews(itemId) {
-		axios
-			.get(`http://localhost:3002/api/items/${itemId}/reviews`)
-			.then((response) => {
-				this.setState({
-					reviews: response.data,
-				});
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}
+  getReviews(itemId) {
+    axios
+      .get(`http://localhost:3002/api/items/${itemId}/reviews`)
+      .then((response) => {
+        this.setState({
+          reviews: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-	render() {
-		console.log(this.state.reviews);
-		return <div>Main Component</div>;
-	}
+  render() {
+    console.log(this.state.reviews);
+    return <div>Main Component</div>;
+  }
 }
 
 export default Main;
