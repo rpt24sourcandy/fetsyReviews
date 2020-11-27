d6f004ae1be098e1b3c154902ed6ee99078f4a39
Tina-Luk <tina.luk.tina@gmail.com>
2020-11-27 15:55:29 -0800

    complete nav review style

--- client/src/components/reviewsNav.jsx
+++ client/src/components/reviewsNav.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Menu, Segment, Label } from 'semantic-ui-react';
 
 class ReviewsNav extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			activeItem: 'reviews for this item',
+		};
 	}
 	render() {
 		return (
 			<div>
-				Reviews for this item {this.props.reviews.length} <br />
-				Review for this shop 1,088
+				<Menu pointing secondary>
+					<Menu.Item name="reviews for this item" active={this.state.activeItem === 'reviews for this item'} onClick={this.handleItemClick}>
+						Reviews for this item
+						<Label as="a">{this.props.reviews.length}</Label>
+					</Menu.Item>
+					<Menu.Item name="reviews for this shop" active={this.state.activeItem === 'reviews for this shop'} onClick={this.handleItemClick}>
+						Reviews for this shop
+						<Label as="a">{this.props.reviews.length + 1000}</Label>
+					</Menu.Item>
+				</Menu>
 				<div>Sort by: Recommended</div>
 			</div>
 		);
--- client/src/components/singleReview.jsx
+++ client/src/components/singleReview.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Rating } from 'semantic-ui-react';
 
 class SingleReview extends React.Component {
 	constructor(props) {
@@ -9,7 +10,7 @@ class SingleReview extends React.Component {
 			<div style={{ border: '2px solid black' }}>
 				{this.props.review.customer_name} <br />
 				{this.props.review.date_of_review} <br />
-				{this.props.review.rating} star <br />
+				<Rating icon="star" rating={this.props.review.rating} defaultRating={3} maxRating={5} size="huge" disabled /> <br />
 				Mask color: {this.props.review.item_option} <br />
 				{this.props.review.review_content} <br />
 			</div>
