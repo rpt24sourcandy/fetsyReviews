e23e577d65ee8413441fe8d63c2496c43ab559d2
Tina-Luk <tina.luk.tina@gmail.com>
2020-11-27 15:26:58 -0800

    complete total review style

--- client/src/components/totalReview.jsx
+++ client/src/components/totalReview.jsx
@@ -10,12 +10,16 @@ class TotalReview extends React.Component {
 		for (let i = 0; i < this.props.reviews.length; i++) {
 			ratingSum += this.props.reviews[i].rating;
 		}
+		let ratingAvg = ratingSum / this.props.reviews.length;
 		return (
 			<div>
-				<Rating icon="star" defaultRating={3} maxRating={5} />
-				<div>
-					{this.props.reviews.length} reviews {ratingSum / this.props.reviews.length} rating stars
-				</div>
+				<h3 className="total-reviews">
+					{(this.props.reviews.length + 1000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} reviews{' '}
+					<span>
+						{' '}
+						<Rating icon="star" rating={Math.ceil(ratingAvg)} defaultRating={3} maxRating={5} size="huge" disabled />{' '}
+					</span>
+				</h3>
 			</div>
 		);
 	}
