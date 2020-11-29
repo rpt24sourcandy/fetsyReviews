305f7a8b74afaaefd2c3844f3b48a81f05bc644c
Tina-Luk <tina.luk.tina@gmail.com>
2020-11-29 13:07:12 -0800

    Add pagination style

--- client/src/components/dataReviews.jsx
+++ client/src/components/dataReviews.jsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import ReviewsNav from './reviewsNav.jsx';
 import SingleReview from './singleReview.jsx';
-import { Feed, Icon } from 'semantic-ui-react';
+import { Feed, Pagination } from 'semantic-ui-react';
 
 class DataReviews extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			page: 1,
+			totalPages: Math.ceil(this.props.reviews.length / 5),
+		};
+		this.handlePageChange = this.handlePageChange.bind(this);
 	}
+
+	componentDidUpdate(prevProps) {
+		if (this.props.reviews !== prevProps.reviews) {
+			this.setState({
+				totalPages: Math.ceil(this.props.reviews.length / 5),
+			});
+		}
+	}
+
+	handlePageChange(e, data) {
+		this.setState({
+			page: data.activePage,
+		});
+	}
+
 	render() {
+		const visibleReviews = this.props.reviews.slice((this.state.page - 1) * 5, (this.state.page - 1) * 5 + 5);
 		return (
 			<div>
 				<ReviewsNav reviews={this.props.reviews} />
 				<div>
 					<Feed>
 						{this.props.reviews.length > 0
-							? this.props.reviews.map((review) => {
+							? visibleReviews.map((review) => {
 									return <SingleReview review={review} key={review.id} />;
 							  })
 							: ''}
 					</Feed>
+					<Pagination onPageChange={this.handlePageChange} defaultActivePage={1} totalPages={this.state.totalPages} boundaryRange={0} ellipsisItem={undefined} siblingRange={1} firstItem={null} lastItem={null} />
 				</div>
+				<br />
 			</div>
 		);
 	}
